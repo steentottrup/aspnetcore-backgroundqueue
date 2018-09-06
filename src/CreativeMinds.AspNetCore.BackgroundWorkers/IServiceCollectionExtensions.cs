1757fe4634f9c497632f486c445fbe47e7f0009c
Steen F. Tøttrup <sft@creativeminds.dk>
2018-09-06 14:17:53 +0200

    adding service collection extensions

---
+++ src/CreativeMinds.AspNetCore.BackgroundWorkers/IServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace CreativeMinds.AspNetCore.BackgroundWorkers {
+
+	public static class IServiceCollectionExtensions {
+
+		public static IServiceCollection AddBackgroundTaskQueue<THostedService, TBackgroundTaskQueue>(this IServiceCollection services) where THostedService : BackgroundService where TBackgroundTaskQueue : class, IBackgroundTaskQueue {
+			services
+				.AddHostedService<THostedService>();
+			services
+				.AddSingleton<IBackgroundTaskQueue, TBackgroundTaskQueue>();
+
+			return services;
+		}
+	}
+}
